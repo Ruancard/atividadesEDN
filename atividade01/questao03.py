df9926ea0e14a27ab1afec2df56befb7046ce4e3
ruancard <ruan.lave12@gmail.com>
2025-08-19 10:21:42 -0300

    Adiconando atividade01/questao03.py

---
+++ atividade01/questao03.py
@@ -0,0 +1,10 @@
+##variaveis de medida
+comprimento = 12
+largura = 14
+altura = 20
+
+##calculando volume
+volume = comprimento * largura * altura
+
+##resultado
+print(f"O volume da caixa é: {volume} cm³")

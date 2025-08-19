c022581683a4f129391669e1dced6327e1199e42
ruancard <ruan.lave12@gmail.com>
2025-08-19 10:38:14 -0300

    Adicionando atividade01/questao04.py

---
+++ atividade01/questoes04.py
@@ -0,0 +1,10 @@
+##variaveis
+nome= "Cadeira Infantil"
+preco = 12.40
+quantidade = 3
+
+##Calculo total
+total= (preco*quantidade)
+
+##Resultado
+print(f"Produto: {nome}\nPreço unitario: {preco:.2f} \nQuantidade: {quantidade} \nTotal: {total:.2f}")

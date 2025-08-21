71adb335d9a13a9a2e1c89458c860e53b84f518b
ruancard <ruan.lave12@gmail.com>
2025-08-21 09:48:59 -0300

    Adicionando atividade02/questao01.py

---
+++ atividade02/questao01.py
@@ -0,0 +1,10 @@
+from currency_converter import CurrencyConverter
+c = CurrencyConverter()
+
+
+valor_em_real = float(input("digite o valor em real: "))
+
+valor_em_dolar = c.convert(valor_em_real, 'BRL', 'USD')
+valor_em_euro = c.convert(valor_em_real, 'BRL', 'EUR')
+
+print(f"valor em dolar: {valor_em_dolar:.2f} \nvalor em euro: {valor_em_euro:.2f}")

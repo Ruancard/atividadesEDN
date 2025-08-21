fc71086d7e3b0a7264c67dffc6a3ff9c69c1e476
ruancard <ruan.lave12@gmail.com>
2025-08-21 09:58:57 -0300

    Adicionando atividade02/questao02.py

---
+++ atividade02/questao02.py
@@ -0,0 +1,11 @@
+##variaveis
+nome = "Camiseta"
+preco_original = 50.00
+porcentagem_desconto = 20
+
+##Calculos
+valor_desconto = ((porcentagem_desconto / 100) * preco_original)
+preco_final = (preco_original - valor_desconto)
+
+##Resultado
+print(f"Preço final: R${preco_final:.2f}")

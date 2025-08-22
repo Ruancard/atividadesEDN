bdb2a33acd02bb4ed402a55f59ef7366fbfa6e26
ruancard <ruan.lave12@gmail.com>
2025-08-22 10:44:36 -0300

    Adicionando atividade03/questao04.py

---
+++ atividade03/questao04.py
@@ -0,0 +1,14 @@
+#Variaveis
+ano = int(input("Digite um ano: "))
+
+#Condicionais
+if ano % 4 == 0: 
+    if ano % 100 == 0:
+        if ano % 400 == 0:
+            print(f"O ano {ano} é bissexto")
+        else:
+            print(f"O ano {ano} não é bissexto")
+    else:
+        print(f"O ano {ano} é bissexto")
+else:
+    print(f"O ano {ano} não é bissexto")

529154bdf1b17e28dc8c3a137a6833acde096374
ruancard <ruan.lave12@gmail.com>
2025-08-26 09:36:11 -0300

    Corrigindo e adicionando comentarios

--- complementares/atividade_par_e_impar.py
+++ complementares/atividade_par_e_impar.py
@@ -1,6 +1,8 @@
+#variaveis
 pares = 0
 impares = 0
 
+#Laço de repetição
 while True:
     try:
         entrada = input('Digite um numero inteiro ou "fim" para sair: ')
@@ -9,12 +11,19 @@ while True:
             break
         numero = int(entrada)
 
+        #Contabiliza numeros pares
         if numero % 2 == 0:
             print(f"o numero {numero} é par")
             pares += 1
+
+        #Contabiliza numeros impares
         if numero % 2 != 0:
             print(f"o numero {numero} é impar")
             impares += 1
+
+    #Tratativa de erros        
     except ValueError:
         print("Digite um caracter valido")
+
+#Resultado
 print(f"foram digitados {pares} numeros pares e {impares} numeros impares.")
--- complementares/atividade_senhas.py
+++ complementares/atividade_senhas.py
@@ -1,12 +1,25 @@
+#Laço de repetição
 while True:
     try:
-        senha = input("Digite a senha: ")
-        if senha.lower() == "sair":
+        senha = input('Digite a senha ou "fim" para encerrar o programa: ')
+        if senha.lower() == "fim":
             print("saindo do programa... ")
             break
-        if len(senha) <= 8:
+        if len(senha) < 8:
             raise Exception("senha muito curta")
+        
+        tem_numero = False
+        for caractere in senha:
+            if caractere in '0123456789':
+                tem_numero = True
+                break
+
+        if tem_numero == False:
+            raise Exception("A senha deve conter pelo menos um número.")
+        
         else:
             print("sua senha é boa")
+
+    #Tratativa de erros
     except Exception as e:
         print(f'erro: "{e}"')
